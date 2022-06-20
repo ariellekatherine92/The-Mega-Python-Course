e399bda7dabedbe6aa48dc5a671ed22da7dc0a45
Ariel Steger <arielsteger@Ariels-MacBook-Air.local>
2022-06-20 14:34:41 -0400

    got pandas to work

--- practice/file.py
+++ practice/file.py
@@ -1,13 +1,12 @@
+import pandas
 import time
 import os
-import pandas
+
 
 while True:
     if os.path.exists("practice/temps_today.csv"):
-        data = pandas.read_csv("files/temps_today.csv")
+        data = pandas.read_csv("practice/temps_today.csv")
         print(data.mean())
-        with open("practice/veggies.txt") as file:
-            print(file.read())
     else:
         print("File does not exist.")
     time.sleep(10)
